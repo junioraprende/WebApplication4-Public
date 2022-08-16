f2f141886ea4bbcb0840caf9960649a2ccaf80ab
Junior Juarez <junmariodesk@outlook.com>
2022-08-16 08:43:13 -0600

    Editar

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;//para validaciones usamos DataAnnotations de .net para hacerlo y no ocupar javascript
 
 namespace WebApplication4.Models
 {
@@ -11,7 +11,9 @@ namespace WebApplication4.Models
         public string NombreCategoria { get; set; }
 
 		[Required(ErrorMessage ="Orden es Obligatorio")]
-		[Range(1, int.MaxValue, ErrorMessage ="El Orden debe ser Mayor a cero.")]    
+		[Range(1, int.MaxValue, ErrorMessage ="El Orden debe ser Mayor a cero.")] 
+        //Range(numeroMenor,numeroMayor) o usando anteriormente para indicar el valor inicia de 1 y que permite el maximo valor
+        //soportador por int, si no mostrar un mensaje de error.
         public int MostrarOrden { get; set; }
     }
 }
