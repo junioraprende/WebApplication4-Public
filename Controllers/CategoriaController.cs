3f1c6f35071ab4a8f278544121d82c2f732b24c8
Junior Juarez <junmariodesk@outlook.com>
2022-08-16 04:24:36 -0600

    martes 16

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -36,11 +36,26 @@ namespace WebApplication4.Controllers
 			{
                 _db.Categoria.Add(categoria);//Cargo los datos recibidos de la vista a la base de datos
                 _db.SaveChanges();//Con este párametro queda grabado en la base de datos
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index)); //finalizado la grabacion de cambio, regreso a la vista principal de index Categoria
             }
             return View(categoria); //en caso de dar un error regresa a la vista sin guardar cambios,
                                     //esto gracias al uso ModelState.IsValid
         }
-        
+
+
+        //Get Editar
+        public IActionResult Editar(int? Id) //Esperamos el párametro Id de tipo int la ? es valores nulos
+        {
+            if (Id==null || Id==0) //validadmos en caso ser nulo o igual a cero nos diga que nos encontro la solicitud
+            {
+                return NotFound();
+            }
+            var obj =_db.Categoria.Find(Id); //Mediante la clase Find(párametro) Entity nos busca el valor de la base de datos
+            if (obj==null)
+            {
+                return NotFound();
+            }
+            return View(obj); //Si encontramos algo en la vista mediante el id, lo traemos a la vista del usuario
+        }
     }
 }
